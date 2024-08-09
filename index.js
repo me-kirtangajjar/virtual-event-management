8663f7a1c62ed96f6d1293fb87e1453ad4c310fd
Kirtan Gajjar <mekirtangajjar@gmail.com>
2024-08-09 22:49:45 +0530

    db setup

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require("express");
+const mongoose = require("mongoose");
 require("dotenv").config();
 
 const app = express();
 
 app.use(express.json());
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/test")
+  .then(() => console.log("DB Connected Successfully"))
+  .catch((err) => console.log(err));
+
 app.use("/", require("./routes/routes"));
 
 app.listen(process.env.PORT, (err) => {
