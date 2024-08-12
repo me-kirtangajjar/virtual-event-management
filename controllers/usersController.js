72517c5b09b061604fba77a52749fa4333010917
Kirtan Gajjar <mekirtangajjar@gmail.com>
2024-08-12 12:34:05 +0530

    implemented jwt & added login api

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,4 +1,5 @@
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const usersModel = require("../models/usersModel");
 
 const usersRegister = async (req, res) => {
@@ -9,16 +10,51 @@ const usersRegister = async (req, res) => {
     if (isUserExist) {
       return res
         .status(400)
-        .send({ message: "Email already exist, try to login." });
+        .send({ message: "Email already exist, try to login" });
     }
 
     const hashPassword = await bcrypt.hash(password, 8);
     await usersModel.create({ email, password: hashPassword, role });
-    return res.send("good");
+    return res.status(201).send({ message: "User registered successfully" });
   } catch (error) {
     console.log(error);
     return res.status(500).send({ message: "Something went wrong" });
   }
 };
 
-module.exports = { usersRegister };
+const usersLogin = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const isUserExist = await usersModel.findOne({ email });
+    if (!isUserExist) {
+      return res
+        .status(404)
+        .send({ message: "User not registered, Try to register first" });
+    }
+
+    const isPasswordCorrect = await bcrypt.compare(
+      password,
+      isUserExist.password
+    );
+    if (!isPasswordCorrect) {
+      return res.status(400).send({ message: "Enter valid credentials" });
+    }
+
+    usersJwtPayload = {};
+
+    return res
+      .status(200)
+      .send({
+        message: "User logged in successfully",
+        Authorization: jwt.sign(usersJwtPayload, process.env.JWTTOKENKEY, {
+          expiresIn: "30d",
+        }),
+      });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send({ message: "Something went wrong" });
+  }
+};
+
+module.exports = { usersRegister, usersLogin };
--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
-const { usersRegister } = require("../controllers/usersController");
+const { usersRegister, usersLogin } = require("../controllers/usersController");
 
+// Users route
 router.post("/users/register", usersRegister);
+router.post("/users/login", usersLogin);
 
 module.exports = router;
